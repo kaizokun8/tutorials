21d6c1ac306096f2386409f8bc519dccd8bbd3f6
sharetute <behi.jeremie@gmail.com>
2021-11-21 16:14:18 +0100

    change auth-serv to localhost, no need to change the etc/host

--- spring-oauth2-resource-client/authorization-server/src/main/java/com/sharetute/config/DefaultSecurityConfig.java
+++ spring-oauth2-resource-client/authorization-server/src/main/java/com/sharetute/config/DefaultSecurityConfig.java
@@ -1,22 +1,14 @@
 package com.sharetute.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-
-import javax.sql.DataSource;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @EnableWebSecurity
 public class DefaultSecurityConfig {
-
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
