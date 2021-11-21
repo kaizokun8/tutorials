25058726201705580cf546ab1a0e7571f5879883
sharetute <behi.jeremie@gmail.com>
2021-11-21 20:42:16 +0100

    issuer must use localhost ...

--- spring-oauth2-resource-client/client-server/src/main/java/com/sharetute/web/ArticlesController.java
+++ spring-oauth2-resource-client/client-server/src/main/java/com/sharetute/web/ArticlesController.java
@@ -1,8 +1,6 @@
 package com.sharetute.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,29 +16,15 @@ public class ArticlesController {
     private WebClient webClient;
 
     @GetMapping(value = {"/", "/articles"})
-    public String[] getArticles(@RegisteredOAuth2AuthorizedClient("client-authorization-code")
-                                        OAuth2AuthorizedClient authorizedClient) {
-
-        System.out.println("<<OAUTH2>>");
-        System.out.println(authorizedClient);
-        System.out.println(authorizedClient.getAccessToken().getTokenType());
-        System.out.println(authorizedClient.getAccessToken().getTokenValue());
-        System.out.println(authorizedClient.getPrincipalName());
-        System.out.println(authorizedClient.getAccessToken().getScopes());
-
+    public String[] getArticles(
+            @RegisteredOAuth2AuthorizedClient("articles-client-authorization-code") OAuth2AuthorizedClient authorizedClient
+    ) {
         return this.webClient
                 .get()
-                .uri("http://localhost:8090/articles")
+                .uri("http://127.0.0.1:8090/articles")
                 .attributes(oauth2AuthorizedClient(authorizedClient))
                 .retrieve()
                 .bodyToMono(String[].class)
                 .block();
-    }
-
-    @GetMapping(value = {"/token"})
-    public String getToken(@RegisteredOAuth2AuthorizedClient("client-authorization-code")
-                                   OAuth2AuthorizedClient authorizedClient) {
-
-        return authorizedClient.getAccessToken().getTokenValue();
     }
 }
